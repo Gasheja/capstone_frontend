4ac0bf62f96e07a53e9b2138ff3c6e331bd3d7fd
Rutagengwa Eric <rutagebanke@gmail.com>
2025-10-11 18:29:19 +0200

    changes

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -11,6 +11,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
+import { useLocation } from "react-router-dom";
 
 export function NavMain({
   items,
@@ -21,6 +22,7 @@ export function NavMain({
     icon?: Icon
   }[]
 }) {
+  const { pathname } = useLocation();
   return (
     <SidebarGroup>
       <SidebarGroupContent className="flex flex-col gap-2">
@@ -46,7 +48,7 @@ export function NavMain({
         <SidebarMenu>
           {items.map((item) => (
             <SidebarMenuItem key={item.title}>
-              <SidebarMenuButton tooltip={item.title}>
+              <SidebarMenuButton tooltip={item.title} isActive={pathname === item.url}>
                 {item.icon && <item.icon />}
                 <span>{item.title}</span>
               </SidebarMenuButton>
