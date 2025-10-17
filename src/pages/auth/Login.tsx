d030ad974379af2def8ead6cdf8d18236ef97b42
Rutagengwa Eric <rutagebanke@gmail.com>
2025-10-17 18:33:39 +0200

    adding changes

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -6,7 +6,7 @@ export default function Login() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
 
-      Congo Decision Support System
+       <h2 className="font-bold text-xl mb-6">Congo Decision Support System</h2>
       <div className="flex w-full max-w-sm flex-col gap-6">
         <LoginForm />
       </div>
